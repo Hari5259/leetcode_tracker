6b56fec8880ce10a7be204c66cfea3ead2f90488
Hari Hara sudhan S <shariharasudhan333@gmail.com>
2026-07-09 11:36:10 +0530

    Create 274-HIndex

---
+++ 274-HIndex/274-HIndex.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 11:36:08 AM
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            int h = n - i;
+
+            if (citations[i] >= h) {
+                return h;
+            }
+        }
+
+        return 0;
+    }
+}
