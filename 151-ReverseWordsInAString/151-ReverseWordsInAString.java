6c210a91a17b9e5fe15192fbc6458d8910fc36c3
Hari Hara sudhan S <shariharasudhan333@gmail.com>
2026-07-09 11:36:26 +0530

    Create 151-ReverseWordsInAString

---
+++ 151-ReverseWordsInAString/151-ReverseWordsInAString.java
@@ -0,0 +1,17 @@
+// Last updated: 7/9/2026, 11:36:24 AM
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.trim().split("\\s+");
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = words.length - 1; i >= 0; i--) {
+            sb.append(words[i]);
+            if (i != 0) {
+                sb.append(" ");
+            }
+        }
+
+        return sb.toString();
+    }
+}
