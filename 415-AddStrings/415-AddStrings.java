3e19e905f1dc6c189a472c70e2d0cc13f364821f
Hari Hara sudhan S <shariharasudhan333@gmail.com>
2026-07-30 09:27:50 +0530

    Create 415-AddStrings

---
+++ 415-AddStrings/415-AddStrings.java
@@ -0,0 +1,29 @@
+// Last updated: 7/30/2026, 9:27:50 AM
+class Solution {
+    public String addStrings(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+
+        StringBuilder ans = new StringBuilder();
+
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int sum = carry;
+
+            if (i >= 0) {
+                sum += num1.charAt(i) - '0';
+                i--;
+            }
+
+            if (j >= 0) {
+                sum += num2.charAt(j) - '0';
+                j--;
+            }
+
+            ans.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return ans.reverse().toString();
+    }
+}
