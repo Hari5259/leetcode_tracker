f43422393cb2316e9e5f94f8d266852576a6c3b7
Hari Hara sudhan S <shariharasudhan333@gmail.com>
2026-09-03 11:38:40 +0530

    Create 2187-MinimumTimetoCompleteTrips

---
+++ 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
@@ -0,0 +1,38 @@
+// Last updated: 9/3/2026, 11:38:38 AM
+1class Solution {
+2    public long minimumTime(int[] time, int totalTrips) {
+3
+4        long left = 1;
+5
+6        long minTime = time[0];
+7
+8        for (int i = 1; i < time.length; i++) {
+9            minTime = Math.min(minTime, time[i]);
+10        }
+11
+12        long right = minTime * totalTrips;
+13
+14        while (left < right) {
+15
+16            long mid = left + (right - left) / 2;
+17
+18            long trips = 0;
+19
+20            for (int i = 0; i < time.length; i++) {
+21                trips += mid / time[i];
+22
+23                if (trips >= totalTrips) {
+24                    break;
+25                }
+26            }
+27
+28            if (trips >= totalTrips) {
+29                right = mid;
+30            } else {
+31                left = mid + 1;
+32            }
+33        }
+34
+35        return left;
+36    }
+37}
